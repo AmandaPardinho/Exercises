9e85867fb0c411e0077d54a7ddf82ef8f0d41c48
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-14 16:36:42 -0300

    Creating the Rent class and part of its code

--- src/example_three/Rent.java
+++ src/example_three/Rent.java
@@ -1,4 +1,17 @@
 package example_three;
 
+import java.util.Scanner;
 public class Rent {
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Student s1 = new Student("João", "joao@joao.com", 7);
+        Student s2 = new Student("Sílvia", "silvia@silvia.com", 0);
+        Student s3 = new Student("Bernardo", "bernardo@bernardo.com", 1);
+        Student s3 = new Student("Giovana", "giovana@giovana.com", 5);
+        Student s3 = new Student("Elsa", "elsa@elsa.com", 9);
+
+        
+
+        scanner.close();
+    }
 }
