01cfec2fbb674cd24a8e5c2a8c4114324a488249
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-14 16:37:19 -0300

    Creating the hostel class and its code

--- src/example_three/Hostel.java
+++ src/example_three/Hostel.java
@@ -1,4 +1,25 @@
 package example_three;
 
 public class Hostel {
+    private int room;
+
+    public Hostel(String name, String email, int room) {
+        super(name, email);
+        this.room = room;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nHostel{" +
+                "room: " + room +
+                "}";
+    }
 }
